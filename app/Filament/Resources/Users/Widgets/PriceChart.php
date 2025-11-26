9256ede5d0d029b38ffffb9c794672f8c2f30313
Josh Embling <joshembling@gmail.com>
2025-11-26 17:30:33 +0000

    update data

--- app/Filament/Resources/Users/Widgets/PriceChart.php
+++ app/Filament/Resources/Users/Widgets/PriceChart.php
@@ -35,21 +35,21 @@ class PriceChart extends ChartWidget
     {
         $activeFilter = $this->filter;
         $range = match ($activeFilter) {
-            '7' => 7,
-            '30' => 30,
-            '60' => 60,
-            '90' => 90,
-            default => '100',
+            '7' => [10, 15, 12, 8, 7, 14, 20, 18, 22, 25, 30, 28],
+            '30' => [120, 140, 108, 200, 202, 25, 30, 28, 26, 24, 22, 20],
+            '60' => [20, 22, 25, 30, 208, 26, 2004, 22, 200, 108, 15, 12],
+            '90' => [300, 208, 206, 204, 22, 20, 18, 15, 12, 10, 8, 7],
+            default => [],
         };
 
         return [
             'datasets' => [
                 [
                     'label' => 'Blog posts created',
-                    'data' => [$range, $range, $range, $range],
+                    'data' => $range,
                 ],
             ],
-            'labels' => ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+            'labels' => $range,
         ];
 
         return [
